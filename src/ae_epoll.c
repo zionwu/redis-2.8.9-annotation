200d17b668f78745a39f323885309e711599f757
zionwu <zionwu3@gmail.com>
2014-06-07 00:56:30 +0800

    add annotation for ae_epoll.c

--- src/ae_epoll.c
+++ src/ae_epoll.c
@@ -31,15 +31,20 @@
 
 #include <sys/epoll.h>
 
+
+//基于epoll的结构体
 typedef struct aeApiState {
     int epfd;
     struct epoll_event *events;
 } aeApiState;
 
+
+//被ae.c中aeCreateEventLoop调用，初始化aeApiState并设为apidata
 static int aeApiCreate(aeEventLoop *eventLoop) {
     aeApiState *state = zmalloc(sizeof(aeApiState));
 
     if (!state) return -1;
+    //为events数组分配setsize大小的内存空间
     state->events = zmalloc(sizeof(struct epoll_event)*eventLoop->setsize);
     if (!state->events) {
         zfree(state);
@@ -55,13 +60,15 @@ static int aeApiCreate(aeEventLoop *eventLoop) {
     return 0;
 }
 
+//被ae.c中aeResizeSetSize调用的函数，根据新的setsize调整aeApiState内数组大小
 static int aeApiResize(aeEventLoop *eventLoop, int setsize) {
     aeApiState *state = eventLoop->apidata;
-
+    //为events数组重新分配内存
     state->events = zrealloc(state->events, sizeof(struct epoll_event)*setsize);
     return 0;
 }
 
+//被aeDeleteEventLoop调用的函数，释放aeApiState的内存
 static void aeApiFree(aeEventLoop *eventLoop) {
     aeApiState *state = eventLoop->apidata;
 
@@ -70,24 +77,29 @@ static void aeApiFree(aeEventLoop *eventLoop) {
     zfree(state);
 }
 
+//被aeCreateFileEvent调用
 static int aeApiAddEvent(aeEventLoop *eventLoop, int fd, int mask) {
     aeApiState *state = eventLoop->apidata;
     struct epoll_event ee;
     /* If the fd was already monitored for some event, we need a MOD
      * operation. Otherwise we need an ADD operation. */
+    //如果事件已存在，将操作符设为MOD。否则为ADD
     int op = eventLoop->events[fd].mask == AE_NONE ?
             EPOLL_CTL_ADD : EPOLL_CTL_MOD;
 
     ee.events = 0;
     mask |= eventLoop->events[fd].mask; /* Merge old events */
+    //根据mask设置events
     if (mask & AE_READABLE) ee.events |= EPOLLIN;
     if (mask & AE_WRITABLE) ee.events |= EPOLLOUT;
     ee.data.u64 = 0; /* avoid valgrind warning */
     ee.data.fd = fd;
+    //调用epoll函数设置
     if (epoll_ctl(state->epfd,op,fd,&ee) == -1) return -1;
     return 0;
 }
 
+//被aeDeleteFileEvent调用，从epoll中删除给定文件描述符
 static void aeApiDelEvent(aeEventLoop *eventLoop, int fd, int delmask) {
     aeApiState *state = eventLoop->apidata;
     struct epoll_event ee;
@@ -99,24 +111,29 @@ static void aeApiDelEvent(aeEventLoop *eventLoop, int fd, int delmask) {
     ee.data.u64 = 0; /* avoid valgrind warning */
     ee.data.fd = fd;
     if (mask != AE_NONE) {
+    	//对应的事件已经存在，op是MOD
         epoll_ctl(state->epfd,EPOLL_CTL_MOD,fd,&ee);
     } else {
+    	//对应的事件并不存在于eventLoop->events[fd]，op为DEL
         /* Note, Kernel < 2.6.9 requires a non null event pointer even for
          * EPOLL_CTL_DEL. */
         epoll_ctl(state->epfd,EPOLL_CTL_DEL,fd,&ee);
     }
 }
 
+//被aeProcessEvents调用，使用epoll查询就绪的文件描述符
 static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     aeApiState *state = eventLoop->apidata;
     int retval, numevents = 0;
 
+    //调用epoll_wait函数，将结果保存在state_events数组中。tvp是阻塞等待的时间
     retval = epoll_wait(state->epfd,state->events,eventLoop->setsize,
             tvp ? (tvp->tv_sec*1000 + tvp->tv_usec/1000) : -1);
     if (retval > 0) {
         int j;
 
         numevents = retval;
+        //根据epoll_wait函数的返回结果，设置eventLoop->fired就绪事件数组
         for (j = 0; j < numevents; j++) {
             int mask = 0;
             struct epoll_event *e = state->events+j;
@@ -132,6 +149,7 @@ static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     return numevents;
 }
 
+//实现多路复用层用的是epoll
 static char *aeApiName(void) {
     return "epoll";
 }
