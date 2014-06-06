473eb16d5b220231db829325785138601481330e
zionwu <zionwu3@gmail.com>
2014-06-07 01:13:42 +0800

    add annotation for ae_select.c

--- src/ae_select.c
+++ src/ae_select.c
@@ -31,6 +31,7 @@
 
 #include <string.h>
 
+//基于select的结构体,用于aeEventLoop的apidata
 typedef struct aeApiState {
     fd_set rfds, wfds;
     /* We need to have a copy of the fd sets as it's not safe to reuse
@@ -38,6 +39,7 @@ typedef struct aeApiState {
     fd_set _rfds, _wfds;
 } aeApiState;
 
+//被ae.c中aeCreateEventLoop调用，初始化aeApiState并设为apidata
 static int aeApiCreate(aeEventLoop *eventLoop) {
     aeApiState *state = zmalloc(sizeof(aeApiState));
 
@@ -48,31 +50,39 @@ static int aeApiCreate(aeEventLoop *eventLoop) {
     return 0;
 }
 
+//被ae.c中aeResizeSetSize调用的函数
 static int aeApiResize(aeEventLoop *eventLoop, int setsize) {
     /* Just ensure we have enough room in the fd_set type. */
+	//如果setsize大于fd_set的最大容量FD_SETSIZE，那么返回-1表示失败
     if (setsize >= FD_SETSIZE) return -1;
     return 0;
 }
 
+//被aeDeleteEventLoop调用的函数，释放aeApiState的内存
 static void aeApiFree(aeEventLoop *eventLoop) {
     zfree(eventLoop->apidata);
 }
 
+//被aeCreateFileEvent调用
 static int aeApiAddEvent(aeEventLoop *eventLoop, int fd, int mask) {
     aeApiState *state = eventLoop->apidata;
 
+    //根据mask添加到不同的fd_set中
     if (mask & AE_READABLE) FD_SET(fd,&state->rfds);
     if (mask & AE_WRITABLE) FD_SET(fd,&state->wfds);
     return 0;
 }
 
+//被aeDeleteFileEvent调用
 static void aeApiDelEvent(aeEventLoop *eventLoop, int fd, int mask) {
     aeApiState *state = eventLoop->apidata;
 
+    //根据mask从不同的fd_set中删除fd
     if (mask & AE_READABLE) FD_CLR(fd,&state->rfds);
     if (mask & AE_WRITABLE) FD_CLR(fd,&state->wfds);
 }
 
+//被aeProcessEvents调用，使用select查询就绪的文件描述符
 static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     aeApiState *state = eventLoop->apidata;
     int retval, j, numevents = 0;
@@ -83,15 +93,20 @@ static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     retval = select(eventLoop->maxfd+1,
                 &state->_rfds,&state->_wfds,NULL,tvp);
     if (retval > 0) {
+    	//遍历eventLoop所有文件事件
         for (j = 0; j <= eventLoop->maxfd; j++) {
             int mask = 0;
             aeFileEvent *fe = &eventLoop->events[j];
-
+            //AE_NONE表示没被使用，跳过
             if (fe->mask == AE_NONE) continue;
+
+            //根据_rfds检查当前fd是否可读
             if (fe->mask & AE_READABLE && FD_ISSET(j,&state->_rfds))
                 mask |= AE_READABLE;
+            //根据_wfds检查当前fd是否可写
             if (fe->mask & AE_WRITABLE && FD_ISSET(j,&state->_wfds))
                 mask |= AE_WRITABLE;
+            //设置就绪事件的值
             eventLoop->fired[numevents].fd = j;
             eventLoop->fired[numevents].mask = mask;
             numevents++;
@@ -100,6 +115,7 @@ static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     return numevents;
 }
 
+//实现多路复用层用的是select
 static char *aeApiName(void) {
     return "select";
 }
